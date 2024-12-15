ca2408f49de8260993d7d347f6d3882e2ddb90d7
virgula <angelo.rosa@protonmail.com>
2024-12-15 18:49:58 +0100

    cleanup of main

--- server/main.go
+++ server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/Virgula0/progetto-dp/server/backend/cmd"
+import (
+	"fmt"
+	"sync"
+
+	"github.com/Virgula0/progetto-dp/server/backend/cmd"
+)
 
 func main() {
-	cmd.RunBackend()
+	// Create a WaitGroup
+	var wg sync.WaitGroup
+
+	// Add one to the WaitGroup for the goroutine
+	wg.Add(1)
+
+	// Start the RunBackend function in a goroutine
+	go func() {
+		defer wg.Done() // Mark this goroutine as done when it completes
+		cmd.RunBackend()
+	}()
+
+	// Wait for backend to finish, it could have crashed so we can kill FE too eventually
+	fmt.Println("Starting backend routine...")
+	wg.Wait()
+	fmt.Println("All tasks completed")
 }
